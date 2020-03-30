fdcb51f57194f0c2f89d350e3bdd6e67e7d32eb3
Duany Dreyton <duanyrf@gmail.com>
2020-03-30 16:21:30 -0300

    fix style mistakes

--- model.go
+++ model.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type Product struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
@@ -21,38 +20,38 @@ func (p *Product) updateProduct(db *sql.DB) error {
 		p.Name, p.Price, p.ID)
 	return err
 }
- func (p *Product) deleteProduct(db *sql.DB) error {
- 	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
- 	return err
- }
+func (p *Product) deleteProduct(db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
+	return err
+}
 
- func (p *Product) createProduct(db *sql.DB) error {
- 	err := db.QueryRow("INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
- 		p.Name, p.Price).Scan(&p.ID)
+func (p *Product) createProduct(db *sql.DB) error {
+	err := db.QueryRow("INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
+		p.Name, p.Price).Scan(&p.ID)
 
- 	if err != nil {
- 		return err
+	if err != nil {
+		return err
 	}
 
- 	return nil
- }
+	return nil
+}
 
- func getProducts(db *sql.DB, start, count int) ([]Product, error) {
- 	rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 OFFSET $2", count, start)
- 	if err != nil {
- 		return nil, err
+func getProducts(db *sql.DB, start, count int) ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 OFFSET $2", count, start)
+	if err != nil {
+		return nil, err
 	}
 
 	defer rows.Close()
 
- 	products := []Product{}
- 	for rows.Next() {
- 		var p Product
- 		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
- 			return nil, err
+	products := []Product{}
+	for rows.Next() {
+		var p Product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
 		}
 		products = append(products, p)
 	}
 
- 	return products, nil
- }
+	return products, nil
+}
