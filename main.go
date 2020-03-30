2f5c300654dd76c7a72c62fbe016fe1224432d6b
Duany Dreyton <duanyrf@gmail.com>
2020-03-30 00:31:59 -0300

    add test code

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ func init() {
 func main() {
 	a := App{}
 	a.Initialize(
-		os.Getenv("")
-)
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+	
+	a.Run(":8010")
 }
